b285d019bc54adda3f7a27198d7fd595e4ad86e0
LiHikoP <114278663+HikoPLi@users.noreply.github.com>
2023-04-10 23:55:44 +0800

    2023-04-11

---
+++ URLrequest.py
@@ -0,0 +1,10 @@
+from urllib.parse import quote
+import requests
+import random
+import hashlib
+
+appid = "20230410001636585"
+secretkey = "L_RILBnzNGqKjDsfvG5C"
+
+
+def requests(inputs):
---
+++ main.py
@@ -0,0 +1,10 @@
+import URLrequest
+
+
+def main():
+    inputs = input("Enter: ")
+    URLrequest.requests(inputs)
+
+
+if "_main_" == "_main_":
+    main()
