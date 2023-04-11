47072463be49d7ed16b23b95d43fe59180c51562
LiHikoP <114278663+HikoPLi@users.noreply.github.com>
2023-04-11 12:43:57 +0800

    2023-04-11

--- URLrequest.py
+++ URLrequest.py
@@ -4,7 +4,37 @@ import random
 import hashlib
 
 appid = "20230410001636585"
-secretkey = "L_RILBnzNGqKjDsfvG5C"
+secretKey = "L_RILBnzNGqKjDsfvG5C"
 
 
-def requests(inputs):
+def get_myurl(words, toLang, fromLang='auto'):
+    for word in words:
+        salt = random.randint(32768, 65536)
+        sign = appid + word + str(salt) + secretKey
+        myMd5 = hashlib.md5()
+        myMd5.update(sign.encode("utf-8"))
+        sign = myMd5.hexdigest()
+        """
+        签名生成方法如下：
+        1、将请求参数中的 APPID(appid), 翻译query(q, 注意为UTF-8编码), 随机数(salt), 以及平台分配的密钥(可在管理控制台查看)
+        按照 appid+q+salt+密钥 的顺序拼接得到字符串1。
+        2、对字符串1做md5，得到32位小写的sign。
+        """
+        yield '/api/trans/vip/translate'+'?appid='+appid+'&q='+quote(word)+'&from='+fromLang+'&to='+toLang+'&salt='+str(salt)+'&sign='+sign
+
+
+def get_translate_word(url):
+    # print('http://api.fanyi.baidu.com' + url)
+    try:
+        response = requests.get('http://api.fanyi.baidu.com' + url, timeout=5)
+        result = response.json()
+        outputJSON = str(result['trans_result'])
+        outputJSON = outputJSON.replace("src", "Input: ")
+        outputJSON = outputJSON.replace("dst", "Output: ")
+        outputJSON = outputJSON.replace("]", "")
+        outputJSON = outputJSON.replace("[", "")
+        outputJSON = outputJSON.replace("}", "")
+        outputJSON = outputJSON.replace("{", "")
+        print(f"{outputJSON}")
+    except Exception as e:
+        print(e)
--- main.py
+++ main.py
@@ -2,9 +2,23 @@ import URLrequest
 
 
 def main():
-    inputs = input("Enter: ")
-    URLrequest.requests(inputs)
+    while "_keep_" == "_keep_":
+        userinput = input(
+            "Enter what you want to translate(Enter 'q' to quite.): ")
+
+        if userinput == "q":
+            print("Thanks for using!")
+            break
+
+        targetLan = input("Target language: ")
+        words = [userinput]
+        for u in URLrequest.get_myurl(words, targetLan):
+            URLrequest.get_translate_word(u)
 
 
 if "_main_" == "_main_":
+    print("http://api.fanyi.baidu.com/doc/21")
+    with open("referenceSheet.txt") as reference:
+        print(str(reference.readline()))
+        print("")
     main()
